7674aa47712309d4ce806988045493d176dcef35
Juliane Pires <mpiresjuliane@outlook.com>
2019-12-12 21:48:19 -0300

    Correção de acento por diferença de charset

--- algoritmo/src/basico/RepositorioPessoa.java
+++ algoritmo/src/basico/RepositorioPessoa.java
@@ -153,21 +153,21 @@ public class RepositorioPessoa {
 		RepositorioPessoa rep = RepositorioPessoa.getInstance();
 		Pessoa MariaLucia = new Pessoa("Maria Lucia da Silva","Inventado");
 		Pessoa MariadasDores = new Pessoa("Maria das Dores Costa","Inventado");
-		Pessoa Carlos = new Pessoa("Carlos Brag√£o","N√£o existe");
-		Pessoa Jose = new Pessoa("Jos√© Rocha","Vit√≥ria");
-		Pessoa Marielly = new Pessoa("Marielly","Ribeir√£o");
+		Pessoa Carlos = new Pessoa("Carlos Brag„o","N„o existe");
+		Pessoa Jose = new Pessoa("JosÈ Rocha","VitÛria");
+		Pessoa Marielly = new Pessoa("Marielly","Ribeir„o");
 		Pessoa Juliane = new Pessoa("Juliane","Inventado");
 		Pessoa Maely = new Pessoa("Maely Coutinho","Socorro");
-		Pessoa MariaGuilia = new Pessoa("Maria Guilia","Vit√≥ria");
+		Pessoa MariaGuilia = new Pessoa("Maria Guilia","VitÛria");
 		Pessoa Fernanda = new Pessoa("Fernanda","Recife");
 		Pessoa Sabrina = new Pessoa("Sabrina","Recife");
 		Pessoa Matheus = new Pessoa("Matheus Felipe","Inventado");
-		Pessoa Victor = new Pessoa("Jo√£o Victor","Olinda");
+		Pessoa Victor = new Pessoa("Jo„o Victor","Olinda");
 		Pessoa Samuel = new Pessoa("Samuel Justino","Recife");
-		Pessoa Rodrigues = new Pessoa("Jos√© Rodrigues","Vit√≥ria");
+		Pessoa Rodrigues = new Pessoa("JosÈ Rodrigues","VitÛria");
 		Pessoa Lucas = new Pessoa("Lucas","Boa Viagem");
 		Pessoa Fernando = new Pessoa("Luiz Fernando","Camaragibe");
-		Pessoa Falcao = new Pessoa("Luiz F. Falc√£o","Recife");
+		Pessoa Falcao = new Pessoa("Luiz F. Falc„o","Recife");
 
 		rep.cadastrar(Falcao);
 		rep.cadastrar(Fernando);
